04a369eebd01b6093eddf675f6a301a2c7a1cb87
Mikle Savchenko <svampx@gmail.com>
2020-02-09 21:19:22 +0200

    first version of collision

---
+++ src/sample/Rectangle.java
@@ -0,0 +1,32 @@
+package sample;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rectangle {
+    private Point leftHigh;
+    private Point rightLow;
+    private List<Point> square = new ArrayList<>();
+
+    public Rectangle(Point leftHigh, Point rightLow) {
+        this.leftHigh = leftHigh;
+        this.rightLow = rightLow;
+    }
+
+    List<Point> getSquare(){
+        for(int i = leftHigh.getX();i <= rightLow.getX(); i++){
+            for (int j = leftHigh.getY(); j >= rightLow.getY(); j--) {
+                square.add(new Point(i,j));
+            }
+        }
+        return square;
+    }
+
+    public void setLeftHigh(Point leftHigh) {
+        this.leftHigh = leftHigh;
+    }
+
+    public void setRightLow(Point rightLow) {
+        this.rightLow = rightLow;
+    }
+}
--- src/sample/Solid.java
+++ src/sample/Solid.java
@@ -1,4 +1,18 @@
 package sample;
 
-public class Solid {
+public class Solid<T extends Solid> {
+    private Rectangle rectangle;
+
+    boolean collision(T obj1, T obj2) {
+        for (Point point : obj1.getRectangle().getSquare()) {
+            if(obj2.getRectangle().getSquare().contains(point)){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Rectangle getRectangle() {
+        return rectangle;
+    }
 }
