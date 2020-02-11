612be1d903fecb3426e22c8cbe18abfbf7a575dc
LadikBeg <vladbehim@gmail.com>
2020-02-11 18:19:44 +0200

    commit

--- src/sample/Tank.java
+++ src/sample/Tank.java
@@ -5,11 +5,11 @@ import sample.Interface.Alive;
 public class Tank implements Alive {
     private final static String  TANK_IMAGE_BLUE = "view/resources/tank_blue.png";
     private final static String  TANK_IMAGE_GREEN = "view/resources/tank_green.png";
-    private final int TANK_HEALTH = 100;
+    private int TANK_HEALTH = 100;
 
     @Override
     public boolean isAlive() {
-        if (TANK_HEALTH < 1)
+        if (TANK_HEALTH > 1)
             return true;
         else
             return false;
